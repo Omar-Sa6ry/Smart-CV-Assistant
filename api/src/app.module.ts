679aea771108d0829cfa942434448da871dc59d6
Omar <omarahmedsabry05@gmail.com>
2026-04-10 09:38:31 +0200

    Apply Graphql, Translation, Graphql, Config from @bts-soft/core Package and test Graphql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
+import {
+  ConfigModule,
+  GraphqlModule,
+  ThrottlerModule,
+  TranslationModule,
+} from '@bts-soft/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule, GraphqlModule, ThrottlerModule, TranslationModule],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,39 @@
-import { NestFactory } from '@nestjs/core';
+// import * as bodyParser from 'body-parser';
+import { json } from 'express';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { I18nValidationException } from 'nestjs-i18n';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+// import { setupInterceptors, setupGraphqlUpload } from '@bts-soft/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4004);
+  try {
+
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+
+    // setupGraphqlUpload(app, 1000000, 1);
+    // setupInterceptors(app);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        stopAtFirstError: true,
+        exceptionFactory: (errors) => new I18nValidationException(errors),
+      }),
+    );
+
+    // app.use('/google/callback', bodyParser.raw({ type: 'application/json' }));
+    app.use(json());
+
+
+
+    await app.listen(process.env.PORT || 4003);
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestException(error);
+  }
 }
+
 bootstrap();
