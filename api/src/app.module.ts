8c4fc45d93c43913e19040cade1e05bd578ceaf7
Omar <omarahmedsabry05@gmail.com>
2026-05-07 21:36:19 +0300

    Fix Final Production Errors on  Hugging Face

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -33,8 +33,11 @@ import { BullModule } from '@nestjs/bullmq';
       connection: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT || '6379', 10),
-        password: process.env.REDIS_PASSWORD,
-        tls: (process.env.REDIS_TLS === 'true' || process.env.REDISTLS === 'true') ? {} : undefined,
+        password: process.env.REDIS_PASSWORD || undefined,
+        tls:
+          process.env.REDIS_TLS === 'true' || process.env.REDISTLS === 'true'
+            ? {}
+            : undefined,
       },
     }),
 
