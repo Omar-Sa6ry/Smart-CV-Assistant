af2949db56e607e883cdfc1fcc5d467852724cd5
Omar <omarahmedsabry05@gmail.com>
2026-05-10 10:37:52 +0300

    Upgrade Ai Model and fix error in upload

--- api/src/modules/analysis/interfaces/analysis-service.interface.ts
+++ api/src/modules/analysis/interfaces/analysis-service.interface.ts
@@ -1,10 +1,10 @@
 import { AnalysisResponse } from '../dtos/analysis.response';
 import { AnalysisHistoryResponse } from '../dtos/analysisHistory.response';
-import { FileUpload } from 'graphql-upload-ts';
+import * as UploadMinimal from 'graphql-upload-minimal';
 
 export interface IAnalysisService {
   triggerAnalysis(cvId: string, userId: string): Promise<AnalysisResponse>;
-  analyzeUploadedCv(file: FileUpload, userId: string): Promise<AnalysisResponse>;
-  getLatestAnalysis(cvId: string, userId: string): Promise<AnalysisResponse>;
-  getAnalysisHistory(cvId: string, userId: string): Promise<AnalysisHistoryResponse>;
+  analyzeUploadedCv(file: UploadMinimal.FileUpload, userId: string): Promise<AnalysisResponse>;
+  getLatestAnalysis(userId: string): Promise<AnalysisResponse>;
+  getAnalysisHistory(userId: string): Promise<AnalysisHistoryResponse>;
 }
